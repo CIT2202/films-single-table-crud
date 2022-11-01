fd01f9e7acc894c1300484a0dd7ecad3b3aec183
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-11-01 08:35:51 +0000

    Update browseable-list.php

--- browseable-list.php
+++ browseable-list.php
@@ -8,10 +8,9 @@ catch (PDOException $exception)
 	echo "Oh no, there was a problem" . $exception->getMessage();
 }
 
-//select all the films
-$query = "SELECT * FROM films";
+$query = "SELECT * FROM films"; //SQL to select all the films
 $resultset = $conn->query($query);
-$films = $resultset->fetchAll();
+$films = $resultset->fetchAll(); //use fetchAll(), we need multiple films
 $conn=NULL;
 
 ?>
@@ -22,12 +21,14 @@ $conn=NULL;
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 </head>
 <body>
-	<ul>
+<ul>
 <?php
 
 //loop over the array of films
 foreach ($films as $film) {
     echo "<li>";
+    // for each film create a hyperlink that features the film's id in the query string
+    // e.g. <a href='details.php?id=2'>Winter's Bone</a>
     echo "<a href='details.php?id={$film["id"]}'>{$film["title"]}</a>";
     echo "</li>";
 }
