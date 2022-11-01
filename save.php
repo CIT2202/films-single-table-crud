5a4068487529b56540bf91ea58a066f650235787
mmantle-hud <m.e.mantle@hud.ac.uk>
2022-11-01 08:40:24 +0000

    Update save.php

--- save.php
+++ save.php
@@ -1,7 +1,7 @@
 <?php
 try{
     $conn = new PDO('mysql:host=localhost;dbname=cit2202', 'cit2202', 'letmein');
-     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 }
 catch (PDOException $exception)
 {
@@ -12,22 +12,23 @@ catch (PDOException $exception)
 //This is a simple example we would normally do some form validation here
 
 //basic form processing
-$title=$_POST['title'];
-$year=$_POST['year'];
-$duration=$_POST['duration'];
-$certId=$_POST['certificate'];
+//look at the form controls in create.php for where these values ($_POST['title'], $_POST['year'] etc.) come from
+$title = $_POST['title'];
+$year = $_POST['year'];
+$duration = $_POST['duration'];
+$certId = $_POST['certificate'];
 
 
 //SQL INSERT for adding a new row
 //Use a prepared statement to bind the values from the form
-$query="INSERT INTO films (id, title, year, duration, certificate_id) VALUES (NULL, :title, :year, :duration, :certId)";
-$stmt=$conn->prepare($query);
+$query = "INSERT INTO films (id, title, year, duration, certificate_id) VALUES (NULL, :title, :year, :duration, :certId)";
+$stmt = $conn->prepare($query);
 $stmt->bindValue(':title', $title);
 $stmt->bindValue(':year', $year);
 $stmt->bindValue(':duration', $duration);
 $stmt->bindValue(':certId', $certId);
 $stmt->execute();
-$conn=NULL;
+$conn = NULL;
 ?>
 
 
